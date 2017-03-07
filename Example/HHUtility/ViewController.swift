6ad0b71b781ee505c85bb8d42a5768241543cc76
Hardik <indianic@ind702.local>
2017-03-07 12:11:49 +0530

    Initial Commit

--- Example/HHUtility/ViewController.swift
+++ Example/HHUtility/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import HHUtility
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let Utility = HHUtility()
+        Utility.showAlertWithTitleMsg(self, title: "Test", message: "This is alert message.")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
